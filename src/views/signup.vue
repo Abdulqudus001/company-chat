c0e2109b492929da5cb3575fedfc4d84aa068a5f
pasDamola <oyindamola.oyetunmibi@outsourceglobal.com>
2020-03-25 06:31:16 -0700

    signup and login page

--- src/views/signup.vue
+++ src/views/signup.vue
@@ -1,12 +1,88 @@
 <template>
-  <v-container>
-    Signup Page
-  </v-container>
+        <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12 mt-12 mx-auto">
+              <v-toolbar
+                color="button"
+                dark
+                flat
+              >
+                <v-toolbar-title>COMPANY CHAT SIGNUP FORM</v-toolbar-title>
+                <v-spacer />
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                    <v-text-field
+                    v-model="user.fullname"
+                    label="Full Name"
+                    name="name"
+                    prepend-icon="mdi-account"
+                    type="text"
+                  />
+                    <v-text-field
+                    v-model="user.phone"
+                    label="Phone"
+                    name="phone"
+                    prepend-icon="mdi-phone"
+                    type="number"
+                  />
+                  <v-text-field
+                    v-model="user.email"
+                    label="Email"
+                    name="email"
+                    prepend-icon="mdi-email"
+                    type="text"
+                  />
+
+                  <v-text-field
+                    v-model="user.password"
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="button" class="white--text">Sign Up</v-btn>
+              </v-card-actions>
+            </v-card>
+            <br>
+            <p class="text-center">Already have an account?
+               <router-link :to="{ name: 'login'}">Log In</router-link>
+            </p>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      user: {
+        email: '',
+        fullname: '',
+        password: '',
+        phone: '',
+      },
+    };
+  },
 };
 </script>
 
